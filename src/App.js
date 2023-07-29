e20b40b9697d3a17f0e618ad639181c4cea961b2
Santosh kumar <127082375+Atharw318@users.noreply.github.com>
2023-07-29 09:46:04 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Item from "./components/Item";
+import Cart from "./components/Cart";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Item name="MacBook Pro" price={100000} />
+      <Item name="Pendrive" price={4000} />
+      <Item name="Mobile" price={35000} />
+      <Cart />
     </div>
   );
 }
